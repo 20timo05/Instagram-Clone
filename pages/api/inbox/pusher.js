4bd525cccbf8d4c9577167876fb3a27a7ea08b42
Timo Rolf <timorolf05@gmail.com>
2023-05-31 20:25:05 +0200

    save online users in json file

--- pages/api/inbox/pusher.js
+++ pages/api/inbox/pusher.js
@@ -1,9 +1,10 @@
 import Pusher from "pusher";
+import fs from "fs";
+import path from "path";
 
 import joinRoom from "../../../lib/pusher/joinRoom";
-import sendMessage from "../../../lib/pusher/sendMessage"
+import sendMessage from "../../../lib/pusher/sendMessage";
 
-export let chatRoomsOnlineUsers = [];
 export const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_KEY,
@@ -12,9 +13,31 @@ export const pusher = new Pusher({
   useTLS: true,
 });
 
+const filePath = path.join(
+  process.cwd(),
+  "/lib/pusher/",
+  "chatRoomsOnlineUsers.json"
+);
+
+function getChatRoomsOnlineUsers() {
+  return JSON.parse(fs.readFileSync(filePath, "utf8"));
+}
+
+function setChatRoomsOnlineUsers(newData) {
+  fs.writeFileSync(filePath, JSON.stringify(newData, null, 2));
+}
+
 export default async function handler(req, res) {
-  const { type, ...data} = req.body;
+  const { type, ...data } = req.body;
 
-  if (type === "join") await joinRoom(req, res, chatRoomsOnlineUsers, data)
+  const chatRoomOnlineMembers = getChatRoomsOnlineUsers();
+  const params = [
+    req,
+    res,
+    data,
+    chatRoomOnlineMembers,
+    setChatRoomsOnlineUsers,
+  ];
 
+  if (type === "join") await joinRoom(...params);
 }
