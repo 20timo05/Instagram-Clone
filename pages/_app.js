7c547350a7447b44eb71154a77963390fa78c7fe
Timo Rolf <timorolf05@gmail.com>
2023-06-07 10:29:19 +0200

    remove strict mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 
@@ -6,13 +5,11 @@ import { AuthContextProvider } from "../store/authContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <StrictMode>
       <SessionProvider session={pageProps.session}>
         <AuthContextProvider>
           <Component {...pageProps} />
         </AuthContextProvider>
       </SessionProvider>
-    </StrictMode>
   );
 }
 
